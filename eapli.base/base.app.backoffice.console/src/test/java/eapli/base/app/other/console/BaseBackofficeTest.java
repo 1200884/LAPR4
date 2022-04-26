fa652e2ba461a23b64ac238f2139f74e62494fd7
1200884 <1200884@isep.ipp.pt>
2022-04-26 15:49:11 +0100

    ENTITIES CREATION

---
+++ eapli.base/base.app.backoffice.console/src/test/java/eapli/base/app/other/console/BaseBackofficeTest.java
@@ -0,0 +1,8 @@
+package eapli.base.base.app.backoffice.console.src.test.java.eapli.base.app.other.console;
+
+/**
+ * Created by Nuno Bettencourt [NMB] on 03/04/16.
+ */
+public class BaseBackofficeTest {
+
+}
---
+++ eapli.base/base.app.other.console/src/test/java/eapli/base/app/other/console/OtherAppTest.java
@@ -0,0 +1,8 @@
+package eapli.base.base.app.other.console.src.test.java.eapli.base.app.other.console;
+
+/**
+ * Created by Nuno Bettencourt [NMB] on 03/04/16.
+ */
+public class OtherAppTest {
+
+}
