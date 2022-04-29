4e2ce0f08543f18e1e7b03cd08360e49015fbf26
SergioFerreira1210829 <1210829@isep.ipp.pt>
2022-04-29 16:27:11 +0100

    Category (incomplete)

---
+++ eapli.base/base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/Domain/Product/CategoryUI.java
@@ -0,0 +1,12 @@
+package eapli.base.app.backoffice.console.Domain.Product;
+
+
+import eapli.framework.io.util.Console;
+import eapli.framework.presentation.console.AbstractUI;
+
+public class CategoryUI  {
+    private final CategoryController categoryController = new CategoryController();
+        public void createnewcategory(){
+
+        }
+}
