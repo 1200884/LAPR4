9d9497b4b488ea0a6d529a26792172f5edaa5932
SergioFerreira1210829 <1210829@isep.ipp.pt>
2022-05-26 16:32:58 +0100

    Methods for FIFO algorithm

--- eapli.base/base.core/src/main/java/eapli/base/AGVmanagement/AGV/application/AGVController.java
+++ eapli.base/base.core/src/main/java/eapli/base/AGVmanagement/AGV/application/AGVController.java
@@ -5,11 +5,12 @@ import eapli.base.AGVmanagement.AGV.domain.Status;
 import eapli.base.infrastructure.persistence.PersistenceContext;
 import eapli.base.modelmanagement.Model.domain.Model;
 import eapli.base.modelmanagement.Model.domain.repositories.ModelRepository;
+import eapli.base.ordermanagement.application.OrderServices;
+import eapli.base.ordermanagement.domain.Orders;
+import eapli.base.ordermanagement.repositories.OrderRepository;
 import eapli.base.warehousemanagement.Domain.Repositories.WarehouseRepository;
 import eapli.base.warehousemanagement.Domain.Warehouse;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class AGVController {
 
@@ -23,10 +24,16 @@ public class AGVController {
 
     private static List<Warehouse> warehouseList = new ArrayList<>();
 
+    private static final OrderServices orderservice = new OrderServices();
+
+    private static OrderRepository orderRepository = PersistenceContext.repositories().Order();
+
+    ArrayList<Orders> queue = new ArrayList<>();
+
     public String createAVG(double maxWeight, String baseLocation, String shortDescription, int modelID) {
         Model model = fetchModel(modelID);
         System.out.println("alfredo");
-        Status status=new Status(2,"null");
+        Status status = new Status(2, "null");
         System.out.println("alberto");
         AGV agv = agvService.createAGV(maxWeight, baseLocation, shortDescription, model);
         return agv.toString();
@@ -75,4 +82,30 @@ public class AGVController {
         }
         return string.toString();
     }
+
+    public void AllOrders() {
+        ArrayList<Orders> orders = (ArrayList<Orders>) orderRepository.findAll();
+
+        for (Orders s : orderRepository.findAll()) {
+            queue.add(s);
+        }
+
+    }
+
+    public void RemoveOneOrder(int order){
+
+        queue.remove(order-1);//Selecionar order-1 porque selecionar numeros entre 1 a total orders
+    }
+
+    public void SizeOrder(){
+        int size = queue.size();
+    }
+
+    public void HeadOfQueue(){
+        int head = queue.indexOf(0);
+    }
+
+
+
+
 }
