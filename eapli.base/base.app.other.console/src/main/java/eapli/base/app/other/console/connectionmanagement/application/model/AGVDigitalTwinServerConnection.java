8660d42f74820c6bd95f22d92b7d761032d0e128
SergioFerreira1210829 <1210829@isep.ipp.pt>
2022-06-17 15:56:54 +0100

    -Creation of the ssl certificate to communicate between agvtwin and agvmanager -US4003 issue #5

--- eapli.base/base.app.other.console/src/main/java/eapli/base/app/other/console/connectionmanagement/application/model/AGVDigitalTwinServerConnection.java
+++ eapli.base/base.app.other.console/src/main/java/eapli/base/app/other/console/connectionmanagement/application/model/AGVDigitalTwinServerConnection.java
@@ -1,19 +1,33 @@
 package eapli.base.app.other.console.connectionmanagement.application.model;
 
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class AGVDigitalTwinServerConnection {
     static Socket[] sock = new Socket[5];
+    static final String KEY_STORE="Documents/ClientAuth/myKeyStore.jks";
+    static final String TRUSTED_STORE="Documents/ClientAuth/myTrustStore.jts";
+    static final String KEYSTORE_PASS="Password1";
 
     private static final int STARTING_AGV_PORT = 125;
     private static final int NUMBER_OF_AGVS = 5;
     public static void main(String args[]) throws Exception {
 
-        ServerSocket[] serverSockets = new ServerSocket[NUMBER_OF_AGVS];
+
+        // Trust these certificates provided by authorized clients
+        System.setProperty("javax.net.ssl.trustStore", TRUSTED_STORE);
+        System.setProperty("javax.net.ssl.trustStorePassword",KEYSTORE_PASS);
+
+        // Use this certificate and private key as server certificate
+        System.setProperty("javax.net.ssl.keyStore",KEY_STORE);
+        System.setProperty("javax.net.ssl.keyStorePassword",KEYSTORE_PASS);
+        SSLServerSocketFactory sslF = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+        SSLServerSocket[] serverSockets = new SSLServerSocket[NUMBER_OF_AGVS];
         for (int i = STARTING_AGV_PORT; i < (STARTING_AGV_PORT+NUMBER_OF_AGVS);i++) {
-            serverSockets[i-STARTING_AGV_PORT] = new ServerSocket(i);
+            serverSockets[i-STARTING_AGV_PORT] = (SSLServerSocket)sslF.createServerSocket(i);
         }
         while(true) {
             for (int i = STARTING_AGV_PORT; i < (STARTING_AGV_PORT+NUMBER_OF_AGVS); i++) {
